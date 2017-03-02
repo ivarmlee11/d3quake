1d09f75f5258cd3f3e62422bb84697c92d9ffbc5
ivarmlee1 <ivarr5@gmail.com>
2017-03-01 22:45:19 -0800

    heroku commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 const job = new CronJob({
-  cronTime: '* */1 * * *',
+  cronTime: '* * */1 * *',
   onTick: () => {
 		request('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&updatedafter=2017-01-02&minmagnitude=5&orderby=magnitude',
 		 (error, response, data) => {
